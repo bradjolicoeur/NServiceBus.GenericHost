8ee958dd6286fe7381ebd4016fc67bc7b143c245
Brad Jolicoeur <brad@jolicoeurconsulting.com>
2019-04-24 08:54:38 -0400

    Initial project

---
+++ NServiceBus.GenericHost.Template/Configuration/Conventions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace NServiceBus.GenericHost.Template.Configuration
+{
+    //TODO: this can be moved to a shared assembly for the system
+    public class NSBConventions
+    {
+
+        public static void ConfigureConventions(ConventionsBuilder conventions)
+        {
+            conventions.DefiningCommandsAs(
+                type =>
+                {
+                    return type.Namespace != null &&
+                            type.Namespace.EndsWith("Commands");
+                });
+            conventions.DefiningEventsAs(
+                type =>
+                {
+                    return type.Namespace != null &&
+                        type.Namespace.EndsWith("Events");
+                });
+            conventions.DefiningMessagesAs(
+                type =>
+                {
+                    return type.Namespace != null &&
+                        type.Namespace.EndsWith("Messages");
+                });
+            conventions.DefiningDataBusPropertiesAs(
+                property =>
+                {
+                    return property.Name.EndsWith("DataBus");
+                });
+            conventions.DefiningExpressMessagesAs(
+                type =>
+                {
+                    return type.Name.EndsWith("Express");
+                });
+            conventions.DefiningTimeToBeReceivedAs(
+                type =>
+                {
+                    if (type.Name.EndsWith("Expires"))
+                    {
+                        return TimeSpan.FromSeconds(30);
+                    }
+                    return TimeSpan.MaxValue;
+                });
+        }
+    }
+}
---
+++ NServiceBus.GenericHost.Template/Configuration/EndpointConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using NServiceBus;
+
+namespace NServiceBus.GenericHost.Template.Configuration
+{
+    public static class EndpointConfigurations
+    {
+        //TODO: this can be moved to a shared assembly for the system
+        public static EndpointConfiguration ConfigureNSB(ServiceCollection serviceCollection, string endpointName)
+        {
+
+            var endpointConfiguration = new EndpointConfiguration(endpointName);
+
+            //Config sections left commented for reference, they will require additional NuGet packages to enable
+            //These are plugins that should be enabled for production ready code
+            #region Monitoring Config...
+            //heartbeat configuration, this is to identify when an endpoint is off or unresponsive
+            //endpointConfiguration.SendHeartbeatTo(
+            //        serviceControlQueue: "particular.servicecontrol",
+            //        frequency: TimeSpan.FromSeconds(15),
+            //        timeToLive: TimeSpan.FromSeconds(30));
+
+            //metrics configuration for servicepulse 
+            //var metrics = endpointConfiguration.EnableMetrics();
+
+            //metrics.SendMetricDataToServiceControl(
+            //    serviceControlMetricsAddress: "particular.monitoring",
+            //    interval: TimeSpan.FromMinutes(1),
+            //    instanceId: "INSTANCE_ID_OPTIONAL");
+
+            //performance counter configuration
+            //var performanceCounters = endpointConfiguration.EnableWindowsPerformanceCounters();
+            //performanceCounters.EnableSLAPerformanceCounters(TimeSpan.FromSeconds(10));
+            #endregion
+
+            //configuring audit queue and error queue
+            endpointConfiguration.AuditProcessedMessagesTo("audit"); //copy of message after processing will go here for servicecontroller
+            endpointConfiguration.SendFailedMessagesTo("error"); //after specified retries is hit, message will be moved here for alerting and recovery
+
+            //TODO: Add production transport
+            var transport = endpointConfiguration.UseTransport<LearningTransport>(); //for production ready, replace with Azure Service Bus, RabbitMQ or MSMQ
+
+            //TODO: Add routing
+            var routing = transport.Routing();
+            //routing.RouteToEndpoint(typeof(ProcessPollingRequest), "PollingRequestSaga");
+            //routing.RegisterPublisher(typeof(ICompletedPollingRequest), "PollingRequestSaga");
+
+            //TODO: Add production persistence
+            endpointConfiguration.UsePersistence<LearningPersistence>(); //for production ready, replace with nHibernate or Azure Storage Provider
+
+            var conventions = endpointConfiguration.Conventions();
+            NSBConventions.ConfigureConventions(conventions); //this will configure message types by the defined conventions
+
+            endpointConfiguration.EnableInstallers(); //This will create transport and peristence objects
+
+            endpointConfiguration.UseContainer<ServicesBuilder>(
+            customizations: customizations =>
+            {
+                customizations.ExistingServices(serviceCollection);
+            });
+
+            return endpointConfiguration;
+
+        }
+    }
+}
